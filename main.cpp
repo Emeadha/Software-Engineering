ebdb221f4f26879f64578b6ea30a9b240f123985
Ethan Harsh <emeadha@gmail.com>
2024-01-20 14:33:29 -0500

    IT WORKS

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ Clock implementation
 #include "plane.h"
 #include "airport.h"
 #include "passenger.h"
+#include <thread>
 
 int main() {
     TimeManager time_manager;
@@ -24,8 +25,26 @@ int main() {
     time_manager.addObserver(&airport);
     time_manager.addObserver(&passenger);
 
-    // Update simulation time
-    time_manager.updateSimulationTime(1, 30, 0);
+    //THIS IS THE MAIN SIMULATION LOOP FOR TIME
+    //24 hours = 1440 minutes
+    //Each iteration is 10 minutes
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+    for(int i = 0; i < 144; i++){
+        // Hold values of hours, minutes, and seconds,
+        // if minutes exceeds 60 add 1 to hours and reset minutes to 0
+        minutes += 10;
+        if(minutes >= 60){
+            hours++;
+            minutes = 0;
+        }
+
+        time_manager.updateSimulationTime(hours, minutes, 0);
+
+        // Sleep for 1 second
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     return 0;
 }
--- plane.cpp
+++ plane.cpp
@@ -10,7 +10,9 @@ Plane object implementation file
 Plane::Plane(const std::string& plane_name) : name(plane_name), current_time(0, 0, 0) {}
 
 void Plane::onTimeUpdate(const Clock& new_time) {
+    //Update the plane's time
     current_time = new_time;
+    //Debugging statement
     std::cout << "Plane " << name << " updated its time to "
               << new_time.hours << ":" << new_time.minutes << ":" << new_time.seconds << std::endl;
 }
