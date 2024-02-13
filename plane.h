0b61a21dcbf4576922b87b9be4d1ccb6ab562c26
RedWriter607 <61717394+redwriter607@users.noreply.github.com>
2024-02-13 17:05:20 -0500

    Minor plane.h fixes, make-and-run shell script
    -Fixed some typos in plane.h
    -Added mrun.sh, a simple shell script to make and then run airline_simulation

--- plane.h
+++ plane.h
@@ -23,7 +23,7 @@ public:
     float Max_fuel; //How much fuel can the plane hold at maximum?
     float Burn_rate; //How much fuel does the plane use up? Liters per kilometer.
     float Current_velocity; //KM per hour
-    float Max_velocity; //Maximum velocity in KM/hour, ideal velocity i.e. what the plane can be expected to spend most of a flight at when not in takeoff/landing is 90% of this. //TODO: Should averageVelocity(Float) be a value in itself? Depends on how often we need to calculate it.
+    float Max_velocity; //Maximum velocity in KM/hour, ideal velocity i.e. what the plane can be expected to spend most of a flight at when not in takeoff/landing is 90% of this.
 
     //Travel distance trackers
     double Odometer; //How many miles the plane has flown, in total.
@@ -32,12 +32,12 @@ public:
 
     //Passenger variables
     int Max_passengers;//How many passengers the plane can carry
-    int Onboard (vector of passenger objects); //Count of passengers //TODO: Make this *actually* a passenger vector, not just an int
+    int Onboard; //Count of passengers //TODO: Make this *actually* a passenger vector, not just an int
     int Count_of_passengers; // Number of passengers onboard
 
     //Availability bools
-    bool In_transit (Bool); //Is the plane in flight?
-    bool Is_operable (Bool); //Can the plane currently be used for transport?
+    bool In_transit; //Is the plane in flight?
+    bool Is_operable; //Can the plane currently be used for transport?
     bool Is_seats_open; //Is the plane not completely full of passengers?
 
     //Costs
@@ -46,6 +46,7 @@ public:
  
     //Location, origin, destination
     double Range; //KILOMETERS of travel possible remaining
+    int Origin_airport_ID; //The airport the plane is starting from on a flight.
     int Target_airport_ID; //The airport the plane is scheduled to land at next.
     double Target_airport_location_distance; // Km to reach airport
     int Target_gate; //The gate of the targeted airport at which the plane is scheduled to land. 
