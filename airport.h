ff0331dbe69f5adb00ddc528454e4adc5dfb07f8
harrisadamgoldstein <147788115+harrisadamgoldstein@users.noreply.github.com>
2024-02-13 21:28:46 -0500

    Update airport.h

--- airport.h
+++ airport.h
@@ -10,19 +10,54 @@ Airport object header file
 
 
 #include "TimeObserver.h"
-#include <string>
+#include "Clock.h"
+#include "timeManager.h"
+#include "gate.h"
+#include "Passenger.h"
 
-using namespace std;   
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Airport : public TimeObserver {
 public:
-    string name;
-    Clock Objects_clock;
+    //Variables
+    int Airport_ID; // Primary key for airport
+    Clock Objects_clock; // Instance of the clock object for the airport, used to keep simulation synchronization
+    string Airport_name; // Airport name
+    TimeManager* time_manager;
+    bool Airport_open; // Whether or not airport is open
 
-    Airport(string airport_name);
+    //Vector for all passenger and gate objects
+    vector<Passenger*> All_passenger_groups;
+    vector<Gate*> All_gates;
+
+    //Constructor
+    Airport(TimeManager *time_manager, string airport_name);
+
+    // Register a passenger_group (to vector) and as an observer
+    void registerPassengerGroup(Passenger* passenger_group);
+
+    // Register a gate (to vector) and as an observer
+    void registerGate(Gate* gate);
 
     // Implement the TimeObserver interface
     virtual void onTimeUpdate(Clock& new_time) override;
+
+    // Getters
+    int getAirportID() const; // Return Airport ID
+    const std::string& getAirportName() const; // Return Airport name
+    bool getAirportOpen() const; // Return whether or not open
+
+    // Setters
+    void setAirportID(int id); // Set Airport ID
+    void setAirportName(const std::string& name); // Set Airport name
+
+    // Multi-action
+    int findGate(int GateID) const; // Finds gate based on Gate_ID, returns the position in vector
 };
+
 
 #endif // AIRPORT_H
