48fed8c7ea73ac23d7b28d06987666e4c16fe2e1
harrisadamgoldstein <147788115+harrisadamgoldstein@users.noreply.github.com>
2024-03-20 17:11:15 -0400

    Update logger.h

--- logger.h
+++ logger.h
@@ -1,57 +1,44 @@
-// logger.h
-#ifndef LOGGER_H
-#define LOGGER_H
-
-#include <string>
-#include <iostream> // Include for cin and cout
-#include "clock.h"  
+#include <iostream>
 #include <fstream>
-#include <iomanip> 
-#include <fstream>
-#include <sstream>
-
+#include "clock.h"
 
 using namespace std;
-class Logger {
-public:
 
-    //Constructor
+class Logger 
+{
+public:
+    // Constructor
     Logger();
 
-    //Destructor
+    // Destructor
     ~Logger();
 
     // Methods to receive updates from planes, flights, and airports
-	void logPlaneUpdate(const std::string& pid, 
-        int p_status, const Clock& first_time, const Clock& second_time);//Creates plane log string, then sends to exportLogs to print to file
-	void logFlightUpdate(const std::string& fid, 
-        int f_status, const Clock& first_time, const Clock& second_time);//Creates flight log string, then sends to exportLogs to print to file
-	void logAirportUpdate(const std::string& aid, int a_status,
-        const Clock& first_time, const Clock& second_time);//Creates airport log string, then sends to exportLogs to print to file
-
-    
-	void exportLogsToFile(int switchCase);//Based on switch case, prints string to appropriate file
-
+    void logPlaneUpdate(const std::string& pid, int p_status, const Clock& first_time, const Clock& second_time);
+    void logFlightUpdate(const std::string& fid, int f_status, const Clock& first_time, const Clock& second_time);
+    void logAirportUpdate(const std::string& aid, int a_status, const Clock& first_time, const Clock& second_time);
 
 private:
-
     // Strings to hold log updates for each entity
     string plane_log;
     string flight_log;
     string airport_log;
 
-    //Ofstream variables
+    // Ofstream variables
     ofstream planeLogFile;
     ofstream flightLogFile;
     ofstream airportLogFile;
 
-    //Names of files ot be printed to
-    string planeFileName = "planeLog.txt";
-    string flightFileName = "flightLog.txt";
-    string airportFileName = "airportLog.txt";
+    // Names of files to be printed to
+    const string planeFileName;
+    const string flightFileName;
+    const string airportFileName;
 
+    // Variables to track whether each type of log has been exported
+    bool plane_exported;
+    bool flight_exported;
+    bool airport_exported;
 
+    // Method to export logs to file based on switchCase
+    void exportLogsToFile(int switchCase);
 };
-
-
-#endif // LOGGER_H
