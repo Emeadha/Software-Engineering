b560be8537167367941e5c7cf78d8234a271d0b5
Ethan Harsh <emeadha@gmail.com>
2024-03-22 13:16:50 -0400

    Revert "Update logger.h"
    This reverts commit b04dc45705fb2a497eae9aa8a19f830c738f3c01.

--- logger.h
+++ logger.h
@@ -1,13 +1,6 @@
-#ifndef LOGGER_H
-#define LOGGER_H
-
 #include <iostream>
 #include <fstream>
 #include "clock.h"
-#include "timeManager.h"
-#include "plane.h"
-#include "flight.h"
-#include "airport.h"
 
 using namespace std;
 
@@ -24,17 +17,12 @@ public:
     void logPlaneUpdate(const std::string& pid, int p_status, const Clock& first_time, const Clock& second_time);
     void logFlightUpdate(const std::string& fid, int f_status, const Clock& first_time, const Clock& second_time);
     void logAirportUpdate(const std::string& aid, int a_status, const Clock& first_time, const Clock& second_time);
-    void setAirportInfo(int airport_ID, const std::string& airport_name);
-    void setPlaneInfo(int plane_ID, const std::string& plane_name);
-    void setFlightInfo(int flight_ID, const std::string& flight_name);
-    void setTimeManager(TimeManager *time_manager); // Add this line
 
 private:
     // Strings to hold log updates for each entity
     string plane_log;
     string flight_log;
     string airport_log;
-    TimeManager* time_manager;
 
     // Ofstream variables
     ofstream planeLogFile;
@@ -57,4 +45,3 @@ private:
     // Pointer to Logger object used for communication
     Logger* log_object;
 };
-#endif // LOGGER_H
