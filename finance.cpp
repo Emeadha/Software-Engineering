6c53a9d58b67048a5080ea673872086f362de191
tcreedle06 <rtylercreedle@gmail.com>
2024-04-03 14:37:07 -0400

    Finance Skeleton Added
    Skeleton for Finance method added. Everything in finance.cpp compiles but I have not tested it and no calls have been implemented outside of it. Makefile also has been updated accordingly.

---
+++ finance.cpp
@@ -0,0 +1,102 @@
+/*
+*   finance.cpp
+*
+*   Implementation of finance.h, used to give a financial summary
+*   of each day, flight, and overall costs and revenue
+*
+*
+*/
+#include "finance.h"
+#include <sstream>
+
+Finance::Finance() : financeFileName("financeLog.txt")
+{
+    financeObj.open(financeFileName);
+}
+
+//Report a cost of a plane to the finance object
+void Finance::reportPlaneCost(int planeID, double value)
+{
+	if(value > 0) 
+	{
+		financeObj << "Plane " << planeID << " reports a cost of: " << value << endl;
+		totalDailyCost += value; 
+		totalOverallCost += value;
+	}
+	else
+	{
+		cerr << "Error! Negative cost not allowed. " << endl;
+	}
+}
+//Report a revenue of a plane to the finance object
+void Finance::reportPlaneRevenue(int planeID, double value)
+{
+	if(value > 0) 
+	{
+		financeObj << "Plane " << planeID << " reports a cost of: " << value << endl;
+		totalDailyRevenue += value;
+		totalOverallRevenue += value; 
+	}
+	else
+	{
+		cerr << "Error! Negative revenue not allowed. " << endl;
+	}
+}
+//Write a day into the financial summary log
+void Finance::reportDay(int day)
+{
+	financeObj << "-----------------------------------------------" << endl;
+	financeObj << "Simulation Day " << day << " Financial Summary" << endl;
+
+	financeObj << "Total Cost: $" << totalDailyCost << endl;
+	financeObj << "Total Revenue: $" << totalDailyRevenue << endl;
+	
+	if(totalDailyCost > totalDailyRevenue) 
+	{
+		financeObj << "A total of $" << totalDailyCost-totalDailyRevenue << " was LOST during day " << day << "." << endl;	
+	}
+	else if(totalDailyRevenue > totalDailyCost)
+	{
+		financeObj << "A total of $" << totalDailyRevenue-totalDailyCost << " was GAINED during day " << day << "." << endl;
+	}
+	else if(totalDailyRevenue == totalDailyCost)
+	{
+		financeObj << "The daily cost and revenue were equal, resulting in a net gain of $0.00. " << endl;
+	}
+	else
+	{
+		cerr << "Unexpected error in reporting daily financial summary. Day: " << day << endl;
+		financeObj << "Error getting difference in revenue and cost." << endl;
+	}
+
+	financeObj << "-----------------------------------------------" << endl;
+}
+//Write the overall report to the financial summary log
+void Finance::reportOverall()
+{
+	financeObj << "-----------------------------------------------" << endl;
+	financeObj << "Simulation Overall Financial Summary" << endl;
+
+	financeObj << "Total Cost: $" << totalOverallCost << endl;
+	financeObj << "Total Revenue: $" << totalOverallRevenue << endl;
+
+	if(totalOverallCost > totalOverallRevenue) 
+	{
+		financeObj << "A total of $" << totalOverallCost-totalOverallRevenue << " was LOST during the simulation." << endl;	
+	}
+	else if(totalOverallRevenue > totalOverallCost)
+	{
+		financeObj << "A total of $" << totalOverallRevenue-totalOverallCost << " was GAINED during the simulation." << endl;
+	}
+	else if(totalOverallRevenue == totalOverallCost)
+	{
+		financeObj << "The daily cost and revenue were equal, resulting in a net gain of $0.00. " << endl;
+	}
+	else
+	{
+		cerr << "Unexpected error in reporting overall financial summary." << endl;
+		financeObj << "Error getting difference in revenue and cost." << endl;
+	}
+	
+	financeObj << "-----------------------------------------------" << endl;
+}
---
+++ finance.h
@@ -0,0 +1,59 @@
+/*
+*   finance.h
+*
+*   Used to create financial summaries of days, 
+*   flights, and total costs and revenue.
+*
+*   Objects will report costs and revenue to the 
+*   finance object so that finance can keep track of 
+*   the overall daily/total costs and revenues.
+*
+*
+*
+*
+*
+*/
+
+#ifndef FINANCE_H
+#define FINANCE_H
+
+#include "timeManager.h"
+#include "plane.h"
+#include "airport.h"
+#include "passenger.h"
+#include "airline.h"
+#include "input.h"
+#include "logger.h"
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <unistd.h>
+
+using namespace std;
+
+class Finance {
+
+    private:
+        double totalDailyCost = 0;
+        double totalDailyRevenue = 0;
+
+        double totalOverallCost = 0;
+        double totalOverallRevenue = 0;
+
+        ofstream financeObj;
+
+        const string financeFileName;
+
+        
+
+    public:
+        Finance();
+
+        void reportPlaneCost(int planeID, double value);
+        void reportPlaneRevenue(int planeID, double value);
+        void reportDay(int day);
+        void reportOverall();
+};
+#endif //FINANCE_H
+
+
